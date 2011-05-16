553fc24a7fcfff55700b29e9ef8d776cd85d7fcb
Jerome Etienne <jerome.etienne@gmail.com>
2011-05-16 08:56:58 +0200

    initial commit

---
+++ test/stacktrace_test.js
@@ -0,0 +1,19 @@
+var logjs	= require('../lib/logjs.js');
+
+function slota2(){
+	//logjs.dir(stacktraceGenerate());
+	//logjs.log("enter", 1+2, slota2);
+	logjs.log("bonjour")	
+	//logjs.assert(false);
+}
+slota2	= logjs.wrapFunction(slota2);
+function slota3(){	slota2();	}
+slota3();
+
+//function slota4(){
+//	logjs.log("prout")
+//	logjs.assert(false, "false is no good")
+//}
+//slota4	= logjs.wrapFunction(slota4);
+//logjs.log("slota4", slota4)
+//slota4();
